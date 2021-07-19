30203d35a57db5aaac28bbc2c087f06ce7781d2f
venkatesh0558 <86581999+venkatesh0558@users.noreply.github.com>
2021-07-19 18:45:28 +0530

    adding get

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
 from flask import *
 import mongodb_vap
 import json
+
 app = Flask(__name__)
 
+@app.route('/entity/create', methods='POST')
+def create():
 
-@app.route('/entity/create', methods=['GET','POST'])
+    if request.method == 'POST':
+        entity_data=request.json
+        res=mongodb_vap.entity_insert(data=entity_data)
+        print(res)
+        return res
+@app.route('/entity/get', methods='GET')
 def create():
     if request.method == 'GET':
         res=mongodb_vap.entity_get()
@@ -12,10 +20,7 @@ def create():
         res_json=json.loads(res)
         res_data={"respond":res_json}
         return res_data
-    elif request.method == 'POST':
-        entity_data=request.json
-        res=mongodb_vap.entity_insert(data=entity_data)
-        print(res)
-        return res
+
 if __name__ == '__main__':
+
     app.run(debug=True)
